c1f9b0fe9afe2c1bec38c6c74176418b8c527d23
jam860 <jam860@uw.edu>
2026-02-13 17:38:34 -0800

    created client files for frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is CommunityCore's Frontend's Homepage. Remember to create a backend as well.</h1>
+    </>
+  )
+}
+
+export default App
