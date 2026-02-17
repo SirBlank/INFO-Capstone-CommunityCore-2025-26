47dd7886626da50b50761e0164bb2de3f5a71a2c
jam860 <jam860@uw.edu>
2026-02-16 18:58:17 -0800

    added some code to visualize header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
+import { Header } from './components/Header.jsx'
 
 function App() {
 
   return (
-    <>
-      <h1>This is CommunityCore's Frontend's Homepage. Remember to create a backend as well.</h1>
-    </>
+    <div class="app">
+      <aside class="sidebar">
+        <div className="logo">
+          <div>⚪</div>
+        </div>
+        <nav>
+          <a class="nav-item active">Programs</a>
+          <a class="nav-item">Users</a>
+          <a class="nav-item">Settings</a>
+        </nav>
+      </aside>
+
+      <div class="main">
+        <header class="header">
+          ISCHOOL MENTORSHIP PROGRAM TRACKER
+        </header>
+
+        <section class="content">
+          <p>Testing</p>
+        </section>
+      </div>
+    </div>
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+export function Header() {
+    <h1>
+        <header>
+            <nav>
+                <div className="nav-container">
+                    <div className="logo" role="button">
+                        <NavLink to="/"><img width="300" height="50" src="/iSchoolSymbol.jpg"alt="iSchool logo" /></NavLink>
+                    </div>
+                    <div className="nav-links desktop-nav">
+                        
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </h1>
+}
